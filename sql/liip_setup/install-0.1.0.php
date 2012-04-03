0b783c8ac62e817303f63d9dfaddb92e34258fb1
Roland Schilter <roland.schilter@liip.ch>
2012-04-03 10:22:32 +0200

    do not blow up if eav_attribute_option.reference does not yet exist

--- sql/liip_setup/install-0.1.0.php
+++ sql/liip_setup/install-0.1.0.php
@@ -5,7 +5,15 @@ $installer = $this;
 
 $installer->startSetup();
 
-$installer->run("ALTER TABLE {$this->getTable('eav_attribute_option')} ADD COLUMN `reference` VARCHAR(255) NOT NULL DEFAULT '';");
+// create eav_attribute_option.`reference` if not yet existing (MySQL only)
+$config = $installer->getConnection()->getConfig();
+$sql = 'SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = :db AND TABLE_NAME = :table AND COLUMN_NAME = :column';
+$binds = array('db' => $config['dbname'], 'table' => $this->getTable('eav_attribute_option'), 'column' => 'reference');
+$stmt = $installer->getConnection()->query($sql, $binds);
+
+if ($stmt->rowCount() == 0) {
+    $installer->run("ALTER TABLE {$this->getTable('eav_attribute_option')} ADD COLUMN `reference` VARCHAR(255) NOT NULL DEFAULT '';");
+}
 
 $installer->endSetup();
 
