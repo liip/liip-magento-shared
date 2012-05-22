0d854f0ca74f2d5cd6857fdafd7d5297170aefc7
Roland Schilter <roland.schilter@liip.ch>
2012-05-22 14:16:54 +0200

    add category helper

---
+++ Helper/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+class Liip_Shared_Helper_Category extends Mage_Core_Helper_Abstract
+{
+    /**
+     * @param string $name Name in the default store view
+     * @return Mage_Catalog_Model_Category|NULL The category or NULL if not found
+     */
+    public function getCategoryByName($name)
+    {
+        $collection = Mage::getModel('catalog/category')->getCollection();
+
+        $collection->setStoreId(Mage_Core_Model_App::ADMIN_STORE_ID);
+        $collection->addAttributeToFilter('name', $name);
+
+        foreach ($collection as $category) {
+            return $category;
+        }
+        return null; // no category found
+    }
+
+    /**
+     * @param string $name Name in the default store view
+     * @return int|NULL The id of the category or NULL if not found
+     */
+    public function getCategoryIdByName($name)
+    {
+        $category = $this->getCategoryByName($name);
+
+        if ($category) {
+            return $category->getId();
+        }
+        return null; // no category found
+    }
+}
+
