f4c865dfad079407e7391074c2e575a7fb7e39bf
Fabian Vogler <fabian@equivalence.ch>
2012-06-01 14:50:15 +0200

    Added getUrl product helper

--- Helper/Product.php
+++ Helper/Product.php
@@ -14,4 +14,20 @@ class Liip_Shared_Helper_Product extends Mage_Core_Helper_Abstract
         $product->load($product->getIdBySku($sku));
         return $product;
     }
+
+    /**
+     * @param Mage_Catalog_Model_Product $product the product model
+     * @return string URL
+     */
+    public function getUrl($product)
+    {
+        $rewrite = null;
+        if ($categories = $product->getCategoryIds()) {
+            $rewrite = Mage::getModel('catalog/url')->getResource()->getRewriteByIdPath('product/' . $product->getId() . '/' . reset($categories), 1);
+        }
+        if (!$rewrite) {
+            $rewrite = Mage::getModel('catalog/url')->getResource()->getRewriteByIdPath('product/' . $product->getId(), 1);
+        }
+        return Mage::getUrl('', array('_direct' => $rewrite->getRequestPath()));
+    }
 }
