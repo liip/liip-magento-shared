5192b36484d86b36518ac3d7b4b57f92271ce2c6
Roland Schilter <roland.schilter@liip.ch>
2012-06-05 10:38:49 +0200

    add liip/config_source_target

---
+++ Model/Config/Source/Target.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Use in your system.xml to specify source_model
+ *
+ * <source_model>liip/config_source_target</source_model>
+ */
+class Jungfrau_Shared_Model_Config_Source_Target
+{
+    const TEST = 'test';
+    const PROD = 'prod';
+
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => self::TEST, 'label' => Mage::helper('liip')->__('Test')),
+            array('value' => self::PROD, 'label' => Mage::helper('liip')->__('Production')),
+        );
+    }
+}
+
