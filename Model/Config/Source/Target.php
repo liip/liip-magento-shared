436f1e44d5afa9f66c39d006d33a586e82a151d1
Roland Schilter <roland.schilter@liip.ch>
2012-07-26 13:05:28 +0200

    Revert "add `dev' option to source target"
    This reverts commit 31ad3634419bd85cf32682ceab1eb504055f0d08.

--- Model/Config/Source/Target.php
+++ Model/Config/Source/Target.php
@@ -9,14 +9,12 @@ class Liip_Shared_Model_Config_Source_Target
 {
     const TEST = 'test';
     const PROD = 'prod';
-    const DEV = 'dev';
 
     public function toOptionArray()
     {
         return array(
             array('value' => self::TEST, 'label' => Mage::helper('liip')->__('Test')),
             array('value' => self::PROD, 'label' => Mage::helper('liip')->__('Production')),
-            array('value' => self::DEV, 'label' => Mage::helper('liip')->__('Development')),
         );
     }
 }
