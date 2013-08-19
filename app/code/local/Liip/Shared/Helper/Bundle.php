cec299dda5b35572172da93b52e3ea37d3ecb737
Benjamin Wohlwend <beni@liip.ch>
2013-08-19 16:00:01 +0200

    fixed a mistake that happened when we merged the filestructure from gitosis

--- app/code/local/Liip/Shared/Helper/Bundle.php
+++ app/code/local/Liip/Shared/Helper/Bundle.php
@@ -3,27 +3,59 @@
 class Liip_Shared_Helper_Bundle extends Mage_Core_Helper_Abstract
 {
     /**
-     * Retrieves the option collection of a bundle including their respective selections.
+     * Calculates the price of a bundle with the given selection.
      *
-     * @param   Mage_Catalog_Model_Product      $product          The product
-     * @param   bool                            $appendAll        Whether to get all selections or just those saleable
-     * @return  array
+     * @param   object|int      The product
+     * @param   array           Bundle item selection
+     * @return  double|false    The price otherwise FALSE if a selection couldn't be found
      */
-    public function getOptions(Mage_Catalog_Model_Product $product, $appendAll = true)
+    public function getPrice($product, $bundleOptions, $extra = array())
     {
-        $typeInstance = $product->getTypeInstance(true);
-        $typeInstance->setStoreFilter($product->getStoreId(), $product);
+        if (!$product instanceof Mage_Catalog_Model_Product_Type_Price) {
+            $product = Mage::getModel('catalog/product')->load($product);
+        }
+        $options = Mage::helper('daytrips/cart')->getOptions($product);
 
-        $optionCollection = $typeInstance->getOptionsCollection($product);
 
-        $optionIds = $typeInstance->getOptionsIds($product);
-        $selectionIds = array();
+        // if user passed all params
+        if (isset($bundleOptions['bundle_option'])) {
+            $bundleOptions = $bundleOptions['bundle_option'];
+        }
 
-        $selectionCollection = $typeInstance->getSelectionsCollection($optionIds, $product);
+        $total = 0;
 
-        $options = $optionCollection->appendSelections($selectionCollection, false, $appendAll);
+        $priceModel = $product->getPriceModel();
+        foreach ($bundleOptions as $optionId => $selectionIds) {
+            $selectionIds = (array)$selectionIds;
 
-        return $options;
+            foreach ($selectionIds as $selectionId) {
+                $selection = $options[$optionId]->getSelectionById($selectionId);
+                if ($selection) {
+                    // calculate special cases
+                    $price = $this->calculateProductPrice($selection, $extra);
+
+                    if ($price === false) {
+                        // default
+                        $price = $priceModel->getSelectionPreFinalPrice($product, $selection, $selection->getSelectionQty());
+                    }
+                    $total += $price;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return $total;
+    }
+
+    protected function calculateProductPrice($selection, $extra = array())
+    {
+        $price = false;
+
+        if (Mage::helper('bergbahn')->isBergbahnTicket($selection) && $selection->getProductType() == Mage_Catalog_Model_Product_Type::TYPE_VIRTUAL) {
+            $price = Mage::helper('jungfrau')->getPrice($selection->getPricematrix(), $extra['date']);
+        }
+
+        return $price;
     }
 }
 
