a112b17e8f3fa60a8f03e20923827dc19974889f
Roland Schilter <roli@schilter.me>
2013-09-13 17:24:07 +0200

    proper umlaut replacements in urls

---
+++ app/code/local/Liip/Shared/Helper/Catalog/Product/Url.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Proper umlaut url replacements
+ */
+class Liip_Shared_Helper_Catalog_Product_Url extends Mage_Catalog_Helper_Product_Url
+{
+    public function __construct()
+    {
+        $this->_convertTable['Ä'] = 'ae';
+        $this->_convertTable['ä'] = 'ae';
+        $this->_convertTable['Ö'] = 'oe';
+        $this->_convertTable['ö'] = 'oe';
+        $this->_convertTable['Ü'] = 'ue';
+        $this->_convertTable['ü'] = 'ue';
+    }
+}
+
