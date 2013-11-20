09a867024bafc17ef9b83fbec07feaacef61978d
Roland Schilter <roli@schilter.me>
2013-11-20 10:13:50 +0100

    call parent ctor for url translation

--- app/code/local/Liip/Shared/Helper/Catalog/Product/Url.php
+++ app/code/local/Liip/Shared/Helper/Catalog/Product/Url.php
@@ -7,6 +7,7 @@ class Liip_Shared_Helper_Catalog_Product_Url extends Mage_Catalog_Helper_Product
 {
     public function __construct()
     {
+        parent::__construct();
         $this->_convertTable['Ä'] = 'ae';
         $this->_convertTable['ä'] = 'ae';
         $this->_convertTable['Ö'] = 'oe';
